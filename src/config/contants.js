814231eb5a0ea34b782147fd7ffb7e4d69de1fb8
Huracanblindado <blindadohuracan@gmail.com>
2026-08-12 19:16:20 +0000

    se agregaron mas codigos para el juego

---
+++ src/config/contants.js
@@ -0,0 +1,14 @@
+export const SCREENS = {
+  ROCK: { id: 'ROCK', name: 'Piedra', icon: '✊🏻'},
+  PAPER: { id: 'PAPER', NAME: 'Papel', icon: '✋🏻'},
+  SCISSORS: { id: 'SCISSORS', name: 'Tijera', icon: '✌🏻'}
+}
+
+export const SCREENS = {
+    START: 'START',
+    MENU: 'MENU',
+    GAME: 'GAME',
+    GAMEOVER: 'GAMEOVER'
+}
+
+export const STORAGE_KEY = 'RPS_SAVED_GAME_STATE'
