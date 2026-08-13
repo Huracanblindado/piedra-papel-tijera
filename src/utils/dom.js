852ad3f40b276c773166b83ba3d5734916539fc1
Huracanblindado <blindadohuracan@gmail.com>
2026-08-13 17:54:51 +0000

    nuevos codigos escritos para el juego

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -4,7 +4,23 @@ const element = document.createElement (tag)
 Object.entries(attributes).forEach(([KeyboardEvent, value]) => {
     if (Key.startswith('on') && typeof value === 'function') {
         element.addEvenlistener(key.substring(2).tolowerCase(), value)
-    }   else if (key === 'className') {    
+    }   else if (key === 'className') { 
+        element.className = value    
+        else {
+        element.setAttribute(key, value)
+    } 
+)}
+
+children.flat(Infinity).forEach(child => {
+    if (child === null || child === undefined || child === false) return 
+    if (typeof child === 'string' || typeof child === 'number') {
+       element.appendChild(documentType.createTextNode(child))
+    }  else if (child instanceof HTMLElement) {
+        element.appendChild(child)
+    }
+})
+
+return element   
 
 export const getRandomChoice = (choices) => {
 
